117580fa5252388bc560159735ecaa6edecfbc4a
Sebixe <72415008+Sebixe@users.noreply.github.com>
2021-06-08 12:33:25 +0200

    Add files via upload

--- api/create.php
+++ api/create.php
@@ -3,51 +3,43 @@
 require 'database-admin.php';
 
 // Récupère les données de création postées dans l'admin
-$postdata = file_get_contents("php://input");
+$postnewdata = file_get_contents("php://input");
 
-if(isset($postdata) && !empty($postdata))
+if(isset($postnewdata) && !empty($postnewdata))
 {
   // Extrait les données 
-  $request = json_decode($postdata);
-
-
-  // Validation des données
-  if(trim($request->date) === '' || trim($request->presentation) === '' || trim($request->img) === '' )
-  {
-    return http_response_code(400);
-  }
+  $request = json_decode($postnewdata);
 
   $date = mysqli_real_escape_string($con, trim($request->date));
-  $presentation = mysqli_real_escape_string($con, trim($request->presentation));
-  $img = mysqli_real_escape_string($con, trim($request->img));
+  $description = mysqli_real_escape_string($con, trim($request->description));
+  $img = trim($request->img);
+  $imageName = trim($request->imageName);
 
+    // L'image est récupérée en Base 64 ainsi que son adresse
+    // Celles ci sont traitées avant d'être encodéees dans la DB
+
+  $imageLink = './assets/'.uniqid().substr($imageName, strrpos($imageName, '\\')+1);
+  $splited = explode(',', $img);
+  $binary = base64_decode($splited[1]);
+  file_put_contents($imageLink, $binary);
 
   // Ajout des données dans la DB "data"
     
-  $sql = "INSERT INTO `data`(`id`,`date`,`presentation`,`img`) VALUES (null,'{$date}','{$presentation}','{$img}')";
+  $sql = "INSERT INTO `data`(`id`,`date`,`description`,`img`,`imageName`) VALUES (null,'{$date}','{$description}','{$imageLink}','{$imageName}')";
 
   if(mysqli_query($con,$sql))
   {
     http_response_code(201);
     $data = [
       'date' => $date,
-      'presentation' => $presentation,
-      'img' => $img,
+      'description' => $description,
+      'img' => $imageLink,
+      'imageName' => $imageName,
       'id'    => mysqli_insert_id($con)
     ];
     echo json_encode($data);
-  }
-  else
-  {
+  }else{
+    echo mysqli_error($con);
     http_response_code(422);
   }
 }
-
-/*
-Travail en cours pour récupérer l'image
-
-$imageLink = './uploads/'.uniqid().substr($contactForm['imageName'], strrpos($contactForm['imageName'], '\\')+1);
-    $splited = explode(',', $contactForm['image']);
-    $binary = base64_decode($splited[1]);
-    file_put_contents($imageLink, $binary);
-*/
